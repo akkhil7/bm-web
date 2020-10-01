9f38d1ba8e700d8d970e074cff7a13f361995c81
Akhil <akkhil@outlook.com>
2020-10-02 05:00:50 +0530

    add ga on prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
+  plugins: process.env.NODE_ENV === 'production' ? [{ src: '~plugins/ga.js', mode: 'client' }] : [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
