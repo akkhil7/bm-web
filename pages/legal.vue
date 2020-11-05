809e0606e380eecad7c2ae8682b1db9b705a08ea
Akhil <akkhil@outlook.com>
2020-11-06 01:58:40 +0530

    refactor: legal page

--- pages/legal.vue
+++ pages/legal.vue
@@ -1,45 +1,45 @@
 <template>
   <div class="wrapper">
-    <Navbar />
+    <Navbar :inverse="true" :fixed-on-scroll="false" />
     <section>
       <div class="container">
-      <div class="columns">
-        <div class="column is-narrow">
-          <div class="sidebar">
-            <h3
-              v-for="item in items"
-              :key="item.key"
-              @click="selectOption(item.key)"
-              :class="getClass(item.key)"
-            >
-              <span>{{ item.label }}</span>
-            </h3>
+        <div class="columns">
+          <div class="column is-narrow">
+            <div class="sidebar">
+              <h3 v-for="item in items" :key="item.key">
+                <NuxtLink
+                  :to="{ path: `/legal/${item.key}` }"
+                  :class="isActive(item.key)"
+                >
+                  <span>{{ item.label }}</span>
+                </NuxtLink>
+              </h3>
+            </div>
+          </div>
+          <div class="column">
+            <NuxtChild />
           </div>
         </div>
-        <div class="column">
-          <Terms v-if="selected === 'tnc'" />
-          <PrivacyPolicy v-else-if="selected === 'privacy'" />
-          <CookiePolicy v-else-if="selected === 'cookie'" />
-          <Disclaimer v-else />
-        </div>
-      </div>
       </div>
     </section>
+    <Footer />
   </div>
 </template>
 
 <script>
 export default {
   data() {
+    const query = this.$route.query
     return {
       items: [
         {
-          key: 'tnc',
+          key: '',
           label: 'Terms and Conditions',
         },
         {
           key: 'privacy',
           label: 'Privacy Policy',
+          path: 'privacy',
         },
         {
           key: 'cookie',
@@ -50,7 +50,7 @@ export default {
           label: 'Disclaimer',
         },
       ],
-      selected: 'tnc',
+      selected: query.view,
       showMenu: false,
     }
   },
@@ -61,28 +61,35 @@ export default {
     },
   },
   methods: {
-    selectOption(key) {
-      this.selected = key
-    },
     toggleMenu() {
       this.showMenu = !this.showMenu
     },
-    getClass(key) {
-      return this.selected === key ? 'active' : ''
+    isActive(key) {
+      const path = this.$route.path
+      let subPath = path.split('/legal')[1]
+      subPath = subPath[0] === '/' ? subPath.substring(1) : subPath
+
+      if (subPath === '' && key === '') {
+        return 'active'
+      } else if (subPath.includes(key)) {
+        if (key === '' || key === '/') {
+          return ''
+        } else {
+          return 'active'
+        }
+      } else return ''
     },
   },
 }
 </script>
 
-
 <style lang="scss" scoped>
 .wrapper {
-  margin-bottom: 2em;
   overflow: hidden;
 }
 
 .sign-in-btn {
-  background-color: rgba(255,255,255,0.1) !important;
+  background-color: rgba(255, 255, 255, 0.1) !important;
   font-weight: 600;
   border: none;
 }
@@ -90,14 +97,20 @@ export default {
 .sidebar {
   padding: 2em;
   padding-left: 0;
-  box-shadow: 10px -10px 10px -5px rgba(0,0,0,0.1);
+  box-shadow: 10px -10px 10px -5px rgba(0, 0, 0, 0.1);
   h3 {
     font-size: 1.5em;
     cursor: pointer;
     margin: 1em 0;
   }
-  h3:hover, .active {
-    color: #635bff;
+  a {
+    color: #4a4a4a;
+    font-weight: 600;
+    transition: all 200ms ease;
+  }
+  .active,
+  a:hover {
+    color: #6030e1;
   }
 }
 
