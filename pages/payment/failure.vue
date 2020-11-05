c3f241feb7bb7e1107e11054826537f4480311f9
Akhil <akkhil@outlook.com>
2020-11-06 01:59:57 +0530

    feat: add payment success/failure pages

---
+++ pages/payment/failure.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: 'PaymentFailure',
+  mounted() {
+    setTimeout(() => {
+      this.$router.push('/pricing')
+    }, 3000)
+  },
+}
+</script>
+
+<template>
+  <div>
+    <Navbar :inverse="true" :fixed-on-scroll="false" />
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <b-message
+            title="Payment failed"
+            type="is-danger"
+            has-icon
+            :closable="false"
+            aria-close-label="Close message"
+          >
+            The payment didn't go through... <br />
+            Please wait while we redirect you back to the pricing page.
+          </b-message>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 100px;
+}
+// .notification {}
+</style>
---
+++ pages/payment/success.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'PaymentFailure',
+  mounted() {
+    const { BM_APP_URI } = process.env
+    setTimeout(() => {
+      window.location.replace(`${BM_APP_URI}/account`)
+    }, 3000)
+  },
+}
+</script>
+
+<template>
+  <div>
+    <Navbar :inverse="true" :fixed-on-scroll="false" />
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <b-message
+            title="Payment confirmed"
+            type="is-success"
+            has-icon
+            :closable="false"
+            aria-close-label="Close message"
+          >
+            Your subscription is confirmed... <br />
+            Please wait while we redirect you back to the dashboard.
+          </b-message>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 100px;
+}
+// .notification {}
+</style>
