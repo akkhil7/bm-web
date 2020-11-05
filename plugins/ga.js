1063590153885a401a92998d62497fd6f800bc15
Akhil <akkhil@outlook.com>
2020-11-06 01:53:57 +0530

    feat: add bookmark recc img

--- plugins/ga.js
+++ plugins/ga.js
@@ -7,7 +7,7 @@ export default ({ app }) => {
      ** Include Google Analytics Script
      */
   ;(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r
+    i.GoogleAnalyticsObject = r
     ;(i[r] =
       i[r] ||
       function () {
